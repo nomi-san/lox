6492e7f83ed025ef0f8d20353a4e173bdd8bd0b8
wy3 <wuuyi123@gmail.com>
2020-01-04 00:08:12 +0700

    update value

--- src/value.c
+++ src/value.c
@@ -1,3 +1,41 @@
 #include <stdio.h>
 
 #include "value.h"
+
+void val_print(val_t value)
+{
+    switch (AS_TYPE(value)) {
+        case VT_NIL:
+            printf("nil");
+            break;
+        case VT_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VT_NUM:
+            printf("%.14g", AS_NUM(value));
+            break;
+        case VT_OBJ:
+            // todo
+            break;
+    }
+}
+
+bool val_equal(val_t a, val_t b)
+{
+    if (AS_TYPE(a) != AS_TYPE(b))
+        return false;
+
+    switch (AS_TYPE(a)) {
+        case VT_NIL:
+            return true;
+        case VT_BOOL:
+            return AS_BOOL(a) == AS_BOOL(b);
+        case VT_NUM:
+            return AS_NUM(a) == AS_NUM(b);
+        case VT_OBJ:
+            // todo
+            ;
+    }
+
+    return false;
+}
--- src/value.h
+++ src/value.h
@@ -21,12 +21,25 @@ typedef struct {
     };
 } val_t;
 
-void val_print(val_t *value);
-
 static const val_t VAL_NIL = { .type = VT_NIL };
-static const val_t VAL_TRUE = { .type = VT_BOOL, .Bool = true }
-static const val_t VAL_FALSE = { .type = VT_BOOL, .Bool = false }
+static const val_t VAL_TRUE = { .type = VT_BOOL, .Bool = true };
+static const val_t VAL_FALSE = { .type = VT_BOOL, .Bool = false };
 
 #define VAL_BOOL(b)     ((val_t){ .type = VT_BOOL, .Bool = (b) })
 #define VAL_NUM(n)      ((val_t){ .type = VT_NUM, .Num = (n) })
-#define VAL_OBJ(o)      ((val_t){ .type = VT_OBJ, .o = (obj_t *)(n) })
+#define VAL_OBJ(o)      ((val_t){ .type = VT_OBJ, .o = (obj_t *)(o) })
+
+#define AS_BOOL(v)      ((v).Bool)
+#define AS_NUM(v)       ((v).Num)
+#define AS_OBJ(v)       ((v).Obj)
+#define AS_TYPE(v)      ((v).type)
+
+#define IS_NIL(v)       (AS_TYPE(v) == VT_NIL)
+#define IS_BOOL(v)      (AS_TYPE(v) == VT_BOOL)
+#define IS_NUM(v)       (AS_TYPE(v) == VT_NUM)
+#define IS_OBJ(v)       (AS_TYPE(v) == VT_OBJ)
+
+#define IS_FALSEY(v)    (!(bool)(v).raw)
+
+void val_print(val_t value);
+bool val_equal(val_t a, val_t b);
