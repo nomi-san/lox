e5ce6cb52b1288b3991071b8c724ddc90be9fd8c
wy3 <wuuyi123@gmail.com>
2020-01-04 01:15:44 +0700

    add vm.c

---
+++ src/vm.c
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "vm.h"
+#include "value.h"
+
+vm_t *vm_create()
+{
+
+}
+
+void vm_close(vm_t *vm)
+{
+
+}
