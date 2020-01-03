9cb08aca6852be5506d78455cbb644bd2d52caf6
wy3 <wuuyi123@gmail.com>
2020-01-03 20:22:49 +0700

    add getting line, column macros

--- src/chunk.h
+++ src/chunk.h
@@ -13,4 +13,6 @@ void chunk_init(chunk_t *chunk);
 void chunk_free(chunk_t *chunk);
 void chunk_emit(chunk_t *chunk, uint8_t byte, int ln, int col);
 
-#define CHUNK_CODEPAGE  256
+#define CHUNK_CODEPAGE      256
+#define CHUNK_GETLN(c, i)   (((c)->lines)[i] >> 16 & 0xFFFF)
+#define CHUNK_GETCOL(c, i)  ((c)->lines)[i] & 0xFFFF)
