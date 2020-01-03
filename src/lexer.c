5612d2c131d649e2dac123e0a934a96ff4ebff70
wy3 <wuuyi123@gmail.com>
2020-01-04 00:52:03 +0700

    lex non-alpha, non-digit characters

--- src/lexer.c
+++ src/lexer.c
@@ -20,6 +20,23 @@ static bool isAtEnd(lexer_t *lexer)
     return *lexer->current == '\0';
 }
 
+static char advance(lexer_t *lexer)
+{
+    lexer->current++;
+    lexer->position++;
+    return lexer->current[-1];
+}
+
+static bool match(lexer_t *lexer, char expected)
+{
+    if (isAtEnd(lexer)) return false;
+    if (*lexer->current != expected) return false;
+
+    lexer->current++;
+    lexer->position++;
+    return true;
+}
+
 static tok_t makeToken(lexer_t *lexer, toktype_t type)
 {
     tok_t token;
@@ -49,6 +66,31 @@ tok_t lexer_scan(lexer_t *lexer)
     lexer->start = lexer->current;
 
     if (isAtEnd(lexer)) return makeToken(lexer, TOKEN_EOF);
+
+    char c = advance(lexer);
+
+    switch (c) {
+        case '(': return makeToken(lexer, TOKEN_LEFT_PAREN);
+        case ')': return makeToken(lexer, TOKEN_RIGHT_PAREN);
+        case '{': return makeToken(lexer, TOKEN_LEFT_BRACE);
+        case '}': return makeToken(lexer, TOKEN_RIGHT_BRACE);
+        case ';': return makeToken(lexer, TOKEN_SEMICOLON);
+        case ',': return makeToken(lexer, TOKEN_COMMA);
+        case '.': return makeToken(lexer, TOKEN_DOT);
+        case '-': return makeToken(lexer, TOKEN_MINUS);
+        case '+': return makeToken(lexer, TOKEN_PLUS);
+        case '/': return makeToken(lexer, TOKEN_SLASH);
+        case '*': return makeToken(lexer, TOKEN_STAR);
+
+        case '!':
+            return makeToken(lexer, match(lexer, '=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        case '=':
+            return makeToken(lexer, match(lexer, '=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+        case '<':
+            return makeToken(lexer, match(lexer, '=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        case '>':
+            return makeToken(lexer, match(lexer, '=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    }
 
     return errorToken(lexer, "Unexpected character.");
 }
