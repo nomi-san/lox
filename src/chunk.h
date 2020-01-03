435510f6fa441a9826c4bea071ba83c60ba8dcda
wy3 <wuuyi123@gmail.com>
2020-01-03 20:12:41 +0700

    add chunk.c

--- src/chunk.h
+++ src/chunk.h
@@ -1,9 +1,14 @@
 #pragma once
 
-typedef struct {
+#include "common.h"
 
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t *code;
+    uint32_t *lines;
 } chunk_t;
 
-void chunk_init();
-void chunk_free();
-void chunk_emit();
+void chunk_init(chunk_t *chunk);
+void chunk_free(chunk_t *chunk);
+void chunk_emit(chunk_t *chunk, uint8_t byte, int line, int column);
