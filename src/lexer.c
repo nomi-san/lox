c79280c9c8591701a56af2301987df120bae8018
wy3 <wuuyi123@gmail.com>
2020-01-04 00:42:58 +0700

    update lexer initializer

--- src/lexer.c
+++ src/lexer.c
@@ -5,5 +5,13 @@
 
 void lexer_init(lexer_t *lexer, vm_t *vm, const char *fname, const char *source)
 {
+    lexer->vm = vm;
+    lexer->fname = fname;
+    lexer->source = source;
 
+    lexer->start = lexer->source;
+    lexer->start = lexer->source;
+    lexer->line = 1;
+    lexer->position = 1;
 }
+
