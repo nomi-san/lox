5ce1e2b0564985b3a969a5e6f8ed234fd07a11e9
wy3 <wuuyi123@gmail.com>
2020-01-04 12:21:10 +0700

    add emitting bytecode

--- src/parser.c
+++ src/parser.c
@@ -8,12 +8,18 @@
 
 typedef struct {
     vm_t *vm;
+    chunk_t *compilingChunk;
     lexer_t *lexer;
     tok_t current;
     tok_t previous;
     bool hadError;
     bool panicMode;
 } parser_t;
+
+static chunk_t *currentChunk(parser_t *parser)
+{
+    return parser->compilingChunk;
+}
 
 static void errorAt(parser_t *parser, tok_t *token, const char *message)
 {
@@ -68,6 +74,56 @@ static void consume(parser_t *parser, toktype_t type, const char* message)
     errorAtCurrent(parser, message);
 }
 
+static void emitByte(parser_t *parser, uint8_t byte)
+{
+    chunk_emit(currentChunk(parser), byte,
+        parser->previous.line, parser->previous.column);
+}
+
+static void emitBytes(parser_t *parser, uint8_t byte1, uint8_t byte2)
+{
+    emitByte(parser, byte1);
+    emitByte(parser, byte2);
+}
+
+static void emitReturn(parser_t *parser)
+{
+    emitByte(parser, OP_RET);
+}
+
+static uint8_t makeConstant(parser_t *parser, val_t value)
+{
+    int constant = arr_add(&currentChunk(parser)->constants, value, false);
+    if (constant > UINT8_MAX) {
+        error(parser, "Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constant;
+}
+
+static void emitConstant(parser_t *parser, val_t value)
+{
+    uint8_t constant = makeConstant(parser, value);
+    emitBytes(parser, OP_CONST, constant);
+}
+
+static void endCompiler(parser_t *parser)
+{
+    emitReturn(parser);
+}
+
+static void number(parser_t *parser)
+{
+    double n = strtod(parser->previous.start, NULL);
+    emitConstant(parser, VAL_NUM(n));
+}
+
+static void expression(parser_t *parser)
+{
+    // What goes here?      
+}
+
 bool compile(vm_t *vm, const char *fname, const char *source, chunk_t *chunk)
 {
     lexer_t lexer;
@@ -75,6 +131,7 @@ bool compile(vm_t *vm, const char *fname, const char *source, chunk_t *chunk)
 
     lexer_init(&lexer, vm, fname, source);
     parser.vm = vm;
+    parser.compilingChunk = chunk;
     parser.lexer = &lexer;
     parser.hadError = false;
     parser.panicMode = false;
@@ -83,5 +140,6 @@ bool compile(vm_t *vm, const char *fname, const char *source, chunk_t *chunk)
     expression(&parser);
     consume(&parser, TOKEN_EOF, "Expect end of expression.");
 
+    endCompiler(&parser);
     return !parser.hadError;
 }
