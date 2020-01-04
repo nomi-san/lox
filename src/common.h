43f912c7ade3210f846621ef1b28eb5af5bbeb17
wy3 <wuuyi123@gmail.com>
2020-01-04 20:41:53 +0700

    add byte combination

--- src/common.h
+++ src/common.h
@@ -4,9 +4,13 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define CMB_BYTES(l, r)	    (uint8_t)(((char)(l) & 0xF) | ((char)(r) & 0xF) << 4)
+#define CMB_LEFT(c)	        (char)(((c) & 0xF))
+#define CMB_RIGHT(c)	    (char)((((c) >> 4) & 0xF))
+
 #define GROW_CAPACITY(x)    ((x) < 8 ? 8 : (x) * 2)
 
-#define STACK_MAX   256
+#define STACK_MAX           256
 
 #define VM_OK               0
 #define VM_COMPILE_ERROR    1
