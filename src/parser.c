202af1b33abe8d13721e0324abda5c6fb89bed9f
wy3 <wuuyi123@gmail.com>
2020-01-04 12:12:32 +0700

    update

--- src/parser.c
+++ src/parser.c
@@ -15,6 +15,37 @@ typedef struct {
     bool panicMode;
 } parser_t;
 
+static void errorAt(parser_t *parser, tok_t *token, const char *message)
+{
+    if (parser->panicMode) return;
+    parser->panicMode = true;
+
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type == TOKEN_ERROR) {
+        // Nothing.                                                
+    }
+    else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser->hadError = true;
+}
+
+static void error(parser_t *parser, const char *message)
+{
+    errorAt(parser, &parser->previous, message);
+}
+
+static void errorAtCurrent(parser_t *parser, const char *message)
+{
+    errorAt(parser, &parser->current, message);
+}
+
 static void advance(parser_t *parser)
 {
     parser->previous = parser->current;
@@ -23,8 +54,18 @@ static void advance(parser_t *parser)
         parser->current = lexer_scan(parser->lexer);
         if (parser->current.type != TOKEN_ERROR) break;
 
-        errorAtCurrent(parser->current.start);
+        errorAtCurrent(parser, parser->current.start);
     }
+}
+
+static void consume(parser_t *parser, toktype_t type, const char* message)
+{
+    if (parser->current.type == type) {
+        advance(parser);
+        return;
+    }
+
+    errorAtCurrent(parser, message);
 }
 
 bool compile(vm_t *vm, const char *fname, const char *source, chunk_t *chunk)
