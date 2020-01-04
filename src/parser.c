737c2f45718fc6d0cc8e5e6c485139c5dfacbf13
wy3 <wuuyi123@gmail.com>
2020-01-04 11:34:09 +0700

    add parser.c

---
+++ src/parser.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "parser.h"
+#include "lexer.h"
+
+bool compile(vm_t *vm, const char *source, chunk_t *chunk)
+{
+
+}
