0bcdb2a33a385dca7e6ffdd0af383eeb8b1acb56
wy3 <wuuyi123@gmail.com>
2020-01-04 12:07:39 +0700

    update

--- src/parser.c
+++ src/parser.c
@@ -6,16 +6,41 @@
 #include "parser.h"
 #include "lexer.h"
 
+typedef struct {
+    vm_t *vm;
+    lexer_t *lexer;
+    tok_t current;
+    tok_t previous;
+    bool hadError;
+    bool panicMode;
+} parser_t;
+
+static void advance(parser_t *parser)
+{
+    parser->previous = parser->current;
+
+    for (;;) {
+        parser->current = lexer_scan(parser->lexer);
+        if (parser->current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser->current.start);
+    }
+}
+
 bool compile(vm_t *vm, const char *fname, const char *source, chunk_t *chunk)
 {
     lexer_t lexer;
     parser_t parser;
 
     lexer_init(&lexer, vm, fname, source);
-
-    advance();
-    expression();
-    consume(TOKEN_EOF, "Expect end of expression.");
+    parser.vm = vm;
+    parser.lexer = &lexer;
+    parser.hadError = false;
+    parser.panicMode = false;
+    
+    advance(&parser);
+    expression(&parser);
+    consume(&parser, TOKEN_EOF, "Expect end of expression.");
 
     return !parser.hadError;
 }
