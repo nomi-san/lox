2b2edec66975327282bd33d0dd8be6ce8e3d1603
wy3 <wuuyi123@gmail.com>
2020-01-05 16:46:27 +0700

    add opcode_tostr()

--- src/chunk.h
+++ src/chunk.h
@@ -62,3 +62,10 @@ void chunk_emit(chunk_t *chunk, uint8_t byte, int ln, int col);
 #define CHUNK_CODEPAGE      256
 #define CHUNK_GETLN(c, i)   (((c)->lines)[i] >> 16 & 0xFFFF)
 #define CHUNK_GETCOL(c, i)  (((c)->lines)[i] & 0xFFFF)
+
+static const char *opcode_tostr(opcode_t opcode) {
+#define _CODE(x) #x,
+    static const char *tab[] = { OPCODES() };
+    return tab[opcode];
+#undef _CODE
+}
