1f06b89a652549a10ff60b8b083d0d04cba5c091
wy3 <wuuyi123@gmail.com>
2020-01-08 23:05:36 +0700

    add lib_thread.c

---
+++ src/lib_thread.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+#include "libs.h"
+#include "vm.h"
+
+void load_libthread(vm_t *vm)
+{
+
+}
--- src/libs.h
+++ src/libs.h
@@ -4,3 +4,4 @@
 #include "vm.h"
 
 void load_libmath(vm_t *vm);
+void load_libthread(vm_t *vm);
