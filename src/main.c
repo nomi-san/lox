edd6546469928435dfa285f1cf8862f28e873fbc
wy3 <wuuyi123@gmail.com>
2020-01-08 23:16:03 +0700

    add lib thread to main

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@ int main(int argc, char **argv)
 
     if (vm != NULL) {
         load_libmath(vm);
+        load_libthread(vm);
         ret = vm_dofile(vm, argv[argc - 1]);
         vm_close(vm);
     }
