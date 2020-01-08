2d5467b0148dcbed87d880f714d52f7cfb8633cd
wy3 <wuuyi123@gmail.com>
2020-01-08 20:08:33 +0700

    add loading math lib to main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
+
 #include "vm.h"
+#include "libs.h"
 
 int main(int argc, char **argv)
 {
@@ -13,6 +15,7 @@ int main(int argc, char **argv)
     int ret = VM_INIT_ERROR;
 
     if (vm != NULL) {
+        load_libmath(vm);
         clock_t clk = clock();
         ret = vm_dofile(vm, argv[argc - 1]);
         printf("\ndone in %.3gs\n", (clock()-clk)/(double)CLOCKS_PER_SEC);
